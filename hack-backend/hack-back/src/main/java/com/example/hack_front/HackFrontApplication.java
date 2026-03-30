0f830eac02dd8fe6cb7bbb5901840c59ae2ce486
Arul <arulravichandran95@gmail.com>
2026-03-30 12:29:46 +0530

    initial commit for new repository: Hackthon_model1

---
+++ hack-backend/hack-back/src/main/java/com/example/hack_front/HackFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.hack_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackFrontApplication.class, args);
+	}
+
+}
---
+++ hack-backend/hack-back/src/test/java/com/example/hack_front/HackFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hack_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
